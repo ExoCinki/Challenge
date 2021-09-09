9fb5fbf497380945e0b3235a78614ef167628350
Aodrën LACOT LE CLANCHE <62881216+ExoCinki@users.noreply.github.com>
2021-09-09 12:28:59 +0200

    [UPDATED] Error Handling

--- main.py
+++ main.py
@@ -4,21 +4,25 @@ import requests
 url = "https://random-data-api.com/api/internet_stuff/random_internet_stuff"
 # Request get on Endpoint
 reponse = requests.get(url)
-# Storage reponse at resquest
-contenu = reponse.json()
-# Split information in variables
-email = contenu["email"]
-username = contenu["username"]
-useragent = contenu["user_agent"]
+if(reponse.status_code == 200):
 
-# Search tag on user_agent and redefine user_agent in emoji
-if ('Windows' in useragent):
-    useragent = "\U0001fa9f"
-elif ('Android' in useragent and 'Linux' in useragent):
-    useragent = "\U0001f916"
-elif ('Linux' in useragent):
-    useragent = "\U0001f427"
-elif ('Macintosh' in useragent or 'Mac OS X' in useragent):
-    useragent = "\U0001f34e"
+    # Storage reponse at resquest
+    contenu = reponse.json()
+    # Split information in variables
+    email = contenu["email"]
+    username = contenu["username"]
+    useragent = contenu["user_agent"]
 
-print("L'adresse email de l'utilisateur",username,"est",email,". Iel utilise le système d'exploitation ",useragent,".")
+    # Search tag on user_agent and redefine user_agent in emoji
+    if ('Windows' in useragent):
+        useragent = "\U0001fa9f"
+    elif ('Android' in useragent and 'Linux' in useragent):
+        useragent = "\U0001f916"
+    elif ('Linux' in useragent):
+        useragent = "\U0001f427"
+    elif ('Macintosh' in useragent or 'Mac OS X' in useragent):
+        useragent = "\U0001f34e"
+
+    print("L'adresse email de l'utilisateur", username, "est", email,". Iel utilise le système d'exploitation ", useragent, ".")
+else:
+    print("Une erreur",reponse.status_code,"est survenue!")
